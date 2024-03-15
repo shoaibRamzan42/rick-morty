95d15b6696729f2d660f09c27660730207bc232f
shoaibRamzan42 <adnanrmzan420@gmail.com>
2024-03-16 01:13:15 +0500

    upaadted

--- src/Road-map.jsx
+++ src/Road-map.jsx
@@ -7,7 +7,7 @@ import road3 from '../src/assets/roadmap-bg3.png'
 
 const RoadMap= () => {
   return (
-    <div id='road' className='flex bg-road bg-cover bg-no-repeat font-Slackey  md:px-12 px-3 py-16 gap-8 flex-col justify-center items-center'>
+    <div id='road' className='flex  font-Slackey  md:px-12 px-3 py-16 gap-8 flex-col justify-center items-center'>
         <h1 className="text-[#74E291] bg-shade bg-no-repeat bg-cover  font-Bold  md:text-[40px] lg:text-[45px] ">RoadMap V2</h1>
         <p className='text-[#74e291] bg-shade bg-no-repeat bg-cover leading-[26px] md:leading-[40px] md:text-[30px] lg:text-[36px] text-center font-medium font-custom'>The ultimate goal of $Rick-morty is to soak up liquidity from the competition!  We’re absorbing all the damp.</p>
         <div className="  flex flex-wrap  justify-center items-center gap-5">
